932efe32ceeb18d206feac4a234f66a6413c87db
Kibet Masai <humphreykibet@gmail.com>
2020-05-30 11:50:24 +0300

    updated aws connection hkm

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:''@localhost/okoa_farmer_db"
 
 #aws
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://ubuntu:Masaki2017$$@localhost/okoa_farmer_db?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Masaki2017$$@localhost/okoa_farmer_db?charset=utf8mb4"
