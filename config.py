552ea92ddf9498bfbfaccdf63cc2d39f83dae17e
Kibet Masai <humphreykibet@gmail.com>
2020-05-30 06:56:42 +0300

    updated travis db connections hkm

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://b2b1802e9376f5:91ac6855@us-cdbr-east-06.cleardb.net/heroku_e9e0456b7084334?charset=utf8mb4"
 
 # travis
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:''@localhost/okoa_farmer_db?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:''@localhost/okoa_farmer_db"
