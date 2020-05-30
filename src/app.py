4bc2378a48c099ec20c993dde0499336539141d9
Kibet Masai <humphreykibet@gmail.com>
2020-05-30 16:59:57 +0300

    updated db port hkm

--- src/app.py
+++ src/app.py
@@ -183,4 +183,4 @@ def facebook_callback():
 #############################################END OF FACEBOOK OAUTH #################################################
 if __name__ == "__main__":
     db.init_app(app)
-    app.run(host='0.0.0.0', port=4000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
