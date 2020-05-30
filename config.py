80fc912f41d8a8b904035e11778a320b3b5f2a90
Kibet Masai <humphreykibet@gmail.com>
2020-05-30 16:37:05 +0300

    changed database connections for heroku hkm

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 # SQLALCHEMY_DATABASE_URI = "mysql://root:Masaki2017$$@localhost/okoa_farmer_db?charset=utf8mb4"
 
 # server
-# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://b2b1802e9376f5:91ac6855@us-cdbr-east-06.cleardb.net/heroku_e9e0456b7084334?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://b2b1802e9376f5:91ac6855@us-cdbr-east-06.cleardb.net/heroku_e9e0456b7084334?charset=utf8mb4"
 
 # travis
 # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:''@localhost/okoa_farmer_db"
 
 #aws
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Masaki2017$$@localhost/okoa_farmer_db?charset=utf8mb4"
+# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Masaki2017$$@localhost/okoa_farmer_db?charset=utf8mb4"
